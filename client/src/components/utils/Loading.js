7b7e468860046305efe36eec66f439a3f173a03e
Antek Kois <koisantek96@gmail.com>
2021-08-07 20:41:50 +0200

    Finish loading component

--- client/src/components/utils/Loading.js
+++ client/src/components/utils/Loading.js
@@ -1,59 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import gsap from 'gsap';
 
-const Loading = ({loadingMessage = 'Ładowanie'}) => {
-  const wrapper = useRef(null);
-
-  useEffect(() => {
-    const element = wrapper.current;
-    const wheel = wrapper.current.children;
-
-    gsap.to(element, {
-      rotateZ: '360deg',
-      duration: 3,
-      ease: 'none',
-      repeat: Infinity,
-    });
-
-    gsap.to(wheel, {
-      ease: 'power4.inOut',
-      repeat: Infinity,
-      keyframes: [
-        { x: '-=100', scale: 0 },
-        { opacity: 1 },
-        { x: '+=50', scale: 1, duration: 0.25 },
-        { x: '+=100', duration: 0.75 },
-        { x: '+=50', scale: 0.5, duration: 0.25 },
-        { x: '-=100', scale: 0.25, duration: 0.25 },
-        { x: '-=100', scale: 0, duration: 0.1 },
-      ],
-      stagger: 0.3,
-    });
-  }, []);
-
-  return (
-    <StyledLoading>
-      <h5>{loadingMessage}</h5>
-      <StyledLoadingScene ref={wrapper}>
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-      </StyledLoadingScene>
-    </StyledLoading>
-  );
-};
+import Spinner from './Spinner';
 
 const StyledLoading = styled.div`
   max-width: 100vw;
@@ -68,33 +16,22 @@ const StyledLoading = styled.div`
   h5 {
     text-transform: uppercase;
     /* background-image: ${({ theme }) => theme.colors.gradient};
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    -moz-background-clip: text;
-    -moz-text-fill-color: transparent; */
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        -moz-background-clip: text;
+        -moz-text-fill-color: transparent; */
     text-align: center;
-
     font-size: ${({ theme }) => theme.typography.sizeH5};
     color: ${({ theme }) => theme.colors.accent};
   }
 `;
-
-const StyledLoadingScene = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  flex-direction: row;
-  width: 100%;
-  height: 180px;
-
-  span {
-    position: absolute;
-    background: ${({ theme }) => theme.colors.accent};
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    opacity: 0;
-  }
-`;
+const Loading = ({ loadingMessage = 'Ładowanie' }) => {
+  return (
+    <StyledLoading>
+      <h5>{loadingMessage}</h5>
+      <Spinner/>
+    </StyledLoading>
+  );
+};
 
 export default Loading;
--- client/src/components/utils/Spinner.js
+++ client/src/components/utils/Spinner.js
@@ -1,38 +1,74 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import styled from 'styled-components';
+import gsap from 'gsap';
+
 
 const StyledSpinner = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  height: 200px;
-`;
+  flex-direction: row;
+  width: 100%;
+  height: 180px;
 
-const StyledSquere = styled.span`
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-  border-radius: 20px;
-  background: ${({ theme }) => theme.colors.gradient};
-  animation: 1s ${({ deley }) => `${deley++}ms`} spinnerAnimation alternate
-    infinite;
-
-  @keyframes spinnerAnimation {
-    0% {
-      transform: translateY(50px);
-    }
-    100% {
-      transform: translateY(-50px);
-    }
+  span {
+    position: absolute;
+    background: ${({ theme }) => theme.colors.accent};
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    opacity: 0;
   }
 `;
 
 const Spinner = () => {
+
+  const spinner = useRef(null);
+
+  useEffect(() => {
+    const element = spinner.current;
+    const wheel = spinner.current.children;
+
+    gsap.to(element, {
+      rotateZ: '360deg',
+      duration: 3,
+      ease: 'none',
+      repeat: Infinity,
+    });
+
+    gsap.to(wheel, {
+      ease: 'power4.inOut',
+      repeat: Infinity,
+      keyframes: [
+        { x: '-=100', scale: 0.3 },
+        { opacity: 1 },
+        { x: '+=50', scale: 1, duration: 0.25 },
+        { x: '+=100', duration: 1 },
+        { x: '+=50', scale: 0.5, duration: 0.25 },
+        { x: '-=100', scale: 0.25, duration: 1 },
+        { x: '-=100', scale: 0.3, duration: 0.25 },
+      ],
+      stagger: 0.4,
+    });
+  }, []);
+
+  
   return (
-    <StyledSpinner >
-      <StyledSquere deley={-200}></StyledSquere>
-      <StyledSquere deley={-100}></StyledSquere>
-      <StyledSquere deley={0}></StyledSquere>
+    <StyledSpinner ref={spinner}>
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
     </StyledSpinner>
   );
 };
